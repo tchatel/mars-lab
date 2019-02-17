2095de2da2d9549593e6cf8e7216997dfd3e9544
Thierry Chatel <tchatel@gmail.com>
2019-02-17 15:47:20 +0100

    étape 01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomePageComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
