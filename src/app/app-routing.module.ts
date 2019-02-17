f67141212513f167735b7a7a5f2f4ecd0ecc8943
Thierry Chatel <tchatel@gmail.com>
2019-02-17 17:12:38 +0100

    étape 04

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomePageComponent} from './home-page/home-page.component';
+import {PageNotFoundComponent} from './core/layout/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: 'home', component: HomePageComponent},
-  {path: '', redirectTo: 'home', pathMatch: 'full'}
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent},
 ];
 
 @NgModule({
--- src/app/core/layout/layout.module.ts
+++ src/app/core/layout/layout.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import {RouterModule} from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, PageNotFoundComponent],
   imports: [
     CommonModule,
     RouterModule,
