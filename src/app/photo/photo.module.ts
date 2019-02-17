47ca93f55808392426db90c9a8aded91f5cf0710
Thierry Chatel <tchatel@gmail.com>
2019-02-17 23:45:04 +0100

    étape 23

--- src/app/photo/photo.module.ts
+++ src/app/photo/photo.module.ts
@@ -4,11 +4,14 @@ import { CommonModule } from '@angular/common';
 import { PhotoRoutingModule } from './photo-routing.module';
 import { PhotoPageComponent } from './photo-page/photo-page.component';
 import { PhotoSearchFormComponent } from './photo-search-form/photo-search-form.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [PhotoPageComponent, PhotoSearchFormComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    // Routing module à la fin :
     PhotoRoutingModule
   ]
 })
